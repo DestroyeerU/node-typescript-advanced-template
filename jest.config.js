da4d2cfa3401e12ed7bf1be5e19e7cc14208d388
DestroyeerU <destroyeeru18@gmail.com>
2020-07-03 00:17:09 -0300

    jest and package.json updates

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
+const { compilerOptions } = require('./tsconfig.json');
+const { pathsToModuleNameMapper } = require('ts-jest');
 
 module.exports = {
   // All imported modules in your tests should be mocked automatically
