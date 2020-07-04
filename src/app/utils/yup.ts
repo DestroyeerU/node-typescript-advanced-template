8aeef4a781aba9293f22ab87db0f9de40dd1bf4a
DestroyeerU <destroyeeru18@gmail.com>
2020-07-04 14:00:43 -0300

    utils/yup updates

--- src/app/utils/yup.ts
+++ src/app/utils/yup.ts
@@ -1,19 +1,11 @@
 import * as Yup from 'yup';
 
-function formatErrors(err: Error | Yup.ValidationError) {
-  if (err instanceof Yup.ValidationError) {
-    return { errors: err.errors };
-  }
-
-  return { error: err };
-}
-
-export async function validateSchema(schema: Yup.ObjectSchema, params: object) {
+export async function validateSchema(schema: Yup.ObjectSchema, params: any) {
   try {
     await schema.validate(params, { abortEarly: false });
 
     return undefined;
-  } catch (exception) {
-    return formatErrors(exception);
+  } catch (err) {
+    return { errors: err.errors };
   }
 }
